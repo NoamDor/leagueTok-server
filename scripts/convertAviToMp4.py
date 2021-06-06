e7de94176b1efeac151f18027b897e69b30b9364
gilad-tamar <giladtamarr@gmail.com>
2021-06-07 00:40:45 +0300

    fix avi to mp4 script

--- scripts/convertAviToMp4.py
+++ scripts/convertAviToMp4.py
@@ -4,4 +4,4 @@ import sys
 avi_file_path = sys.argv[1]
 mp4_file_path = sys.argv[2]
 
-os.popen("ffmpeg -i {input} -ac 2 -b:v 2000k -c:a aac -c:v libx264 -b:a 160k -vprofile high -bf 0 -strict experimental -f mp4 {output}.mp4".format(input = avi_file_path, output = mp4_file_path))
+os.popen("ffmpeg -i {input} -c:v libx264 -preset slow -crf 22 -profile:v baseline -level 3.0 -movflags +faststart -pix_fmt yuv420p -c:a libfdk_aac -b:a 128k -f mp4 {output}.mp4".format(input = avi_file_path, output = mp4_file_path))
