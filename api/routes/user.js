1642a5413b00b55fd6c9520e7c74a007291b4780
gilad-tamar <giladtamarr@gmail.com>
2021-06-10 19:43:57 +0300

    user

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    connect, getAll, get, registerDeviceToken
+    connect, getAll, get, registerDeviceToken, setIsAdmin
 } = require('../controllers/user');
 
 router.post('/sign-up', connect);
 router.get('/all/:lastUpdated', getAll);
 router.get("/:uid", get);
 router.put('/device', registerDeviceToken);
+router.put('/admin', setIsAdmin);
 
 module.exports = router;
